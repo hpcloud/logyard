d48d2d23e41e7a787262a0d2c3ff5112c6536ce9
Sridhar Ratnakumar <sridharr@activestate.com>
2012-10-18 20:34:30 -0700

    fetch-dependencies.sh: reference GOPATH env properly

--- tools/fetch-dependencies.sh
+++ tools/fetch-dependencies.sh
@@ -5,17 +5,17 @@
 PBF=http://mercurial.activestate.com/stackato-mirrors/goprotobuf/
 mkdir -p $GOPATH/src/launchpad.net
 
-git clone -q gitolite@gitolite.activestate.com:tail.git 			$(GOPATH)/src/github.com/srid/tail
+git clone -q gitolite@gitolite.activestate.com:tail.git 			$GOPATH/src/github.com/srid/tail
 
-bzr branch -q lp:tomb 										$(GOPATH)/src/launchpad.net/tomb
-git clone -q https://github.com/ActiveState/fsnotify.git 	$(GOPATH)/src/github.com/howeyc/fsnotify
-hg -q clone $PBF  $(GOPATH)/src/code.google.com/p/goprotobuf
-git clone -q https://github.com/ActiveState/doozer 			$(GOPATH)/src/github.com/ActiveState/doozer
-git clone -q https://github.com/ActiveState/pretty 			$(GOPATH)/src/github.com/kr/pretty
-#git clone -q https://github.com/ActiveState/doozerconfig 	$(GOPATH)/src/github.com/ActiveState/doozerconfig
+bzr branch -q lp:tomb 										$GOPATH/src/launchpad.net/tomb
+git clone -q https://github.com/ActiveState/fsnotify.git 	$GOPATH/src/github.com/howeyc/fsnotify
+hg -q clone $PBF  $GOPATH/src/code.google.com/p/goprotobuf
+git clone -q https://github.com/ActiveState/doozer 			$GOPATH/src/github.com/ActiveState/doozer
+git clone -q https://github.com/ActiveState/pretty 			$GOPATH/src/github.com/kr/pretty
+#git clone -q https://github.com/ActiveState/doozerconfig 	$GOPATH/src/github.com/ActiveState/doozerconfig
 
-git clone -q https://github.com/ActiveState/nats	 		$(GOPATH)/src/github.com/apcera/nats
-#git clone -q https://github.com/ActiveState/radix	 		$(GOPATH)/src/github.com/fzzbt/radix
-git clone -q https://github.com/ActiveState/gouuid	 		$(GOPATH)/src/github.com/nu7hatch/gouuid
-#git clone -q https://github.com/ActiveState/colors.git      $(GOPATH)/src/github.com/wsxiaoys/colors
+git clone -q https://github.com/ActiveState/nats	 		$GOPATH/src/github.com/apcera/nats
+#git clone -q https://github.com/ActiveState/radix	 		$GOPATH/src/github.com/fzzbt/radix
+git clone -q https://github.com/ActiveState/gouuid	 		$GOPATH/src/github.com/nu7hatch/gouuid
+#git clone -q https://github.com/ActiveState/colors.git      $GOPATH/src/github.com/wsxiaoys/colors
 
