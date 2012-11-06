d32bdf6e35de51d6a518e2f280b8cc068bdb0a9d
Sridhar Ratnakumar <sridharr@activestate.com>
2012-11-06 14:52:30 -0800

    kato drain list: sort drains and left-justify

--- cmd/logyardctl/list.go
+++ cmd/logyardctl/list.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"logyard"
+	"sort"
 )
 
 type list struct {
@@ -18,8 +19,20 @@ func (cmd *list) DefineFlags(fs *flag.FlagSet) {
 
 func (cmd *list) Run(args []string) error {
 	Init()
-	for name, uri := range logyard.Config.Drains {
-		fmt.Printf("%20s\t%s\n", name, uri)
+	for _, name := range sortedKeys(logyard.Config.Drains) {
+		uri := logyard.Config.Drains[name]
+		fmt.Printf("%-20s\t%s\n", name, uri)
 	}
 	return nil
+}
+
+func sortedKeys(m map[string]string) []string {
+	keys := make([]string, len(m))
+	idx := 0
+	for key, _ := range m {
+		keys[idx] = key
+		idx++
+	}
+	sort.Strings(keys)
+	return keys
 }
