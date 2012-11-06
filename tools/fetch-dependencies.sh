a16fe61372c39e3390d06db3c07123eb4b36d49b
Sridhar Ratnakumar <sridharr@activestate.com>
2012-11-06 04:03:54 -0800

    use latest fsnotify, will pin to specific commit later

--- tools/fetch-dependencies.sh
+++ tools/fetch-dependencies.sh
@@ -9,20 +9,19 @@ mkdir -p $GOPATH/src/launchpad.net
 
 # fetched during install.sh. FIXME
 # git clone -q gitolite@gitolite.activestate.com:tail 			$GOPATH/src/github.com/srid/tail
-echo "WARNING: tail.git needs to be cloned manually"
+echo "WARNING: tail.git is expected to be cloned manually"
 
+# XXX: need to be mirrored (bug 95841)
 bzr branch -q lp:tomb 										$GOPATH/src/launchpad.net/tomb
-git clone -q https://github.com/ActiveState/fsnotify.git 	$GOPATH/src/github.com/howeyc/fsnotify
+
 hg -q clone $PBF  $GOPATH/src/code.google.com/p/goprotobuf
+
 git clone -q https://github.com/ActiveState/doozer 			$GOPATH/src/github.com/ActiveState/doozer
 git clone -q https://github.com/ActiveState/pretty 			$GOPATH/src/github.com/kr/pretty
 git clone -q https://github.com/ActiveState/gouuid	 		$GOPATH/src/github.com/nu7hatch/gouuid
 
+# XXX: our mirrors are outdated; we trust the owners of these repo.
+#git clone -q https://github.com/ActiveState/fsnotify.git 	$GOPATH/src/github.com/howeyc/fsnotify
 #git clone -q https://github.com/ActiveState/nats    $GOPATH/src/github.com/apcera/nats
-git clone -q https://github.com/apcera/nats	 		$GOPATH/src/github.com/apcera/nats
 #git clone -q https://github.com/ActiveState/doozerconfig 	$GOPATH/src/github.com/srid/doozerconfig
-
-#git clone -q https://github.com/ActiveState/colors.git      $GOPATH/src/github.com/wsxiaoys/colors
-
-git clone -q https://github.com/fzzbt/radix	 		$GOPATH/src/github.com/fzzbt/radix
 #git clone -q https://github.com/ActiveState/radix	 		$GOPATH/src/github.com/fzzbt/radix
