4bf3dcd47ddf09b4eeeca1bce892993285e72423
Sridhar Ratnakumar <sridharr@activestate.com>
2012-11-07 11:52:07 -0800

    drain retry logic: set a bound on exponential wait time = 5 minutes

--- retry.go
+++ retry.go
@@ -5,7 +5,6 @@ import (
 	"time"
 )
 
-// Retryer is a simple retryer
 type Retryer struct {
 	recentAttempts int
 	lastAttempt    time.Time
@@ -15,18 +14,25 @@ func NewRetryer() *Retryer {
 	return new(Retryer)
 }
 
-// Wait appropriately waits until next try
+const MAX_WAIT_SECONDS = 60 * 5 // 5 minutes
+
+// Wait appropriately waits until next try (exponential backoff delay)
 func (retry *Retryer) Wait(msg string) {
 	retry.recentAttempts += 1
 	if retry.recentAttempts > 3 {
+		waitSeconds := retry.recentAttempts
+		if waitSeconds > MAX_WAIT_SECONDS {
+			waitSeconds = MAX_WAIT_SECONDS
+		}
 		log.Printf("%s; retrying after %d seconds...",
-			msg, retry.recentAttempts)
-		time.Sleep(time.Duration(retry.recentAttempts) * time.Second)
+			msg, waitSeconds)
+		time.Sleep(time.Duration(waitSeconds) * time.Second)
 	} else {
 		log.Printf("%s; retrying...", msg)
 	}
 
-	// if the retry happens after a long time, reset our stats
+	// reset our stats if there weren't any retry attempts in the last
+	// minute.
 	if time.Now().Sub(retry.lastAttempt).Seconds() > 60 {
 		log.Println("Resetting retry attempts; ", time.Now().Sub(retry.lastAttempt).Seconds())
 		retry.recentAttempts = 0
