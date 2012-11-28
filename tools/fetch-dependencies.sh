e1d6c22dc7fa6d86c6b3f6b7038937dde0244c24
Sridhar Ratnakumar <sridharr@activestate.com>
2012-11-28 13:21:36 -0800

    doozer client with protobuf fixes. also pin protobuf repo.

--- tools/fetch-dependencies.sh
+++ tools/fetch-dependencies.sh
@@ -18,7 +18,7 @@ function git_get {
 echo "WARNING: tail.git is expected to be cloned manually"
 
 
-git_get https://github.com/ActiveState/doozer $GOPATH/src/github.com/ActiveState/doozer 7d71ee7
+git_get https://github.com/ActiveState/doozer $GOPATH/src/github.com/ActiveState/doozer 4c6b387e
 git_get https://github.com/kr/pretty $GOPATH/src/github.com/kr/pretty 821b30f5
 git_get https://github.com/nu7hatch/gouuid $GOPATH/src/github.com/nu7hatch/gouuid 0345199
 git_get https://github.com/howeyc/fsnotify $GOPATH/src/github.com/howeyc/fsnotify d6220df
@@ -28,8 +28,10 @@ git_get https://github.com/srid/log2 $GOPATH/src/github.com/srid/log2 03c27b9
 git_get https://github.com/fzzbt/radix $GOPATH/src/github.com/fzzbt/radix 7687e823
 git_get https://github.com/alecthomas/gozmq $GOPATH/src/github.com/alecthomas/gozmq 965ec0982
 
-# get protobuf from activestate mirror
-hg -q clone http://mercurial.activestate.com/stackato-mirrors/goprotobuf/ $GOPATH/src/code.google.com/p/goprotobuf
+hg -q clone https://code.google.com/p/goprotobuf/ $GOPATH/src/code.google.com/p/goprotobuf
+pushd $GOPATH/src/code.google.com/p/goprotobuf
+hg update -r 29db96746e5d
+popd
 
 # XXX: need to be mirrored (bug 95841)
 mkdir -p $GOPATH/src/launchpad.net
