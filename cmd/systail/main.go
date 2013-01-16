89dc3e8730104d671cd35d3dc51739ba5268baf0
Sridhar Ratnakumar <sridharr@activestate.com>
2013-01-16 11:23:24 -0800

    systail: split a long function

--- cmd/systail/main.go
+++ cmd/systail/main.go
@@ -2,16 +2,61 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/ActiveState/log"
 	"github.com/ActiveState/tail"
 	"logyard"
-	"net"
 	"os"
 	"stackato/server"
 	"unicode/utf8"
 )
+
+func tailLogFile(
+	name string, filepath string,
+	nodeid string, c *logyard.Client) (*tail.Tail, error) {
+	if filepath == "" {
+		filepath = fmt.Sprintf("/s/logs/%s.log", name)
+	}
+
+	log.Info("Tailing... ", filepath)
+
+	t, err := tail.TailFile(filepath, tail.Config{
+		MaxLineSize: Config.MaxRecordSize,
+		MustExist:   false,
+		Follow:      true,
+		// ignore existing content, to support subsequent re-runs of systail
+		Location: 0,
+		ReOpen:   true,
+		Poll:     false})
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func(name string, tail *tail.Tail) {
+		for line := range tail.Lines {
+			// JSON must be a valid UTF-8 string
+			if !utf8.ValidString(line.Text) {
+				line.Text = string([]rune(line.Text))
+			}
+			data, err := json.Marshal(map[string]interface{}{
+				"UnixTime": line.Time.Unix(),
+				"Text":     line.Text,
+				"Name":     name,
+				"NodeID":   nodeid})
+			if err != nil {
+				tail.Killf("Failed to convert to JSON: %v", err)
+				break
+			}
+			err = c.Send("systail."+name+"."+nodeid, string(data))
+			if err != nil {
+				log.Fatal("Failed to send to logyard: ", err)
+			}
+		}
+	}(name, t)
+
+	return t, nil
+}
 
 func main() {
 	LoadConfig()
@@ -28,47 +73,12 @@ func main() {
 	}
 	tailers := []*tail.Tail{}
 
-	for process, logfile := range PROCESSES {
-		if logfile == "" {
-			logfile = fmt.Sprintf("/s/logs/%s.log", process)
-		}
-
-		log.Info("Tailing... ", logfile)
-		t, err := tail.TailFile(logfile, tail.Config{
-			MaxLineSize: Config.MaxRecordSize,
-			MustExist:   false,
-			Follow:      true,
-			// ignore existing content, to support subsequent re-runs of systail
-			Location: 0,
-			ReOpen:   true,
-			Poll:     false})
+	for name, logfile := range PROCESSES {
+		t, err := tailLogFile(name, logfile, nodeid, c)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
-
 		tailers = append(tailers, t)
-
-		go func(process string, tail *tail.Tail) {
-			for line := range tail.Lines {
-				// JSON must be a valid UTF-8 string
-				if !utf8.ValidString(line.Text) {
-					line.Text = string([]rune(line.Text))
-				}
-				data, err := json.Marshal(map[string]interface{}{
-					"UnixTime": line.Time.Unix(),
-					"Text":     line.Text,
-					"Name":     process,
-					"NodeID":   nodeid})
-				if err != nil {
-					tail.Killf("Failed to convert to JSON: %v", err)
-					break
-				}
-				err = c.Send("systail."+process+"."+nodeid, string(data))
-				if err != nil {
-					log.Fatal("Failed to send to logyard: ", err)
-				}
-			}
-		}(process, t)
 	}
 
 	for _, tail := range tailers {
@@ -82,29 +92,4 @@ func main() {
 	// error.
 
 	os.Exit(1)
-}
-
-func localIP() (net.IP, error) {
-	tt, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-	for _, t := range tt {
-		aa, err := t.Addrs()
-		if err != nil {
-			return nil, err
-		}
-		for _, a := range aa {
-			ipnet, ok := a.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			v4 := ipnet.IP.To4()
-			if v4 == nil || v4[0] == 127 { // loopback address 
-				continue
-			}
-			return v4, nil
-		}
-	}
-	return nil, errors.New("cannot find local IP address")
 }
