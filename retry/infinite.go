cd16c32d2f33ad06203ad31a959c283139f48345
Sridhar Ratnakumar <sridharr@activestate.com>
2013-03-18 18:55:55 -0700

    Bug #98280 - fix infinite retryter to reset itself
    after 20 minutes of no-retry (no disconnection), retryer will reset
    itself so that future retry delays are calculated from 5s -> 5m,
    progressively.

--- retry/infinite.go
+++ retry/infinite.go
@@ -5,39 +5,57 @@ import (
 	"time"
 )
 
+// InfiniteRetryer retries infinitely, but with progressive delays
+// (5secs -> 5mins) that resets itself after 20 minutes of no-retry.
 type InfiniteRetryer struct {
-	started time.Time
+	firstRetry time.Time
+	lastRetry  time.Time
 }
 
 func NewInfiniteRetryer() Retryer {
-	return &InfiniteRetryer{time.Now()}
+	return new(InfiniteRetryer)
 }
 
-// Wait appropriately waits until next try. Wait delay is increased
-// based on the length of failures, but Wait always returns true
-// (hence InfiniteRetryer).
 func (retry *InfiniteRetryer) Wait(msg string, shouldWarn bool) bool {
-	period := time.Now().Sub(retry.started)
 	var delay time.Duration
-	switch {
-	case period < time.Minute:
-		// once every 5 seconds for 1 minute
-		delay = 5 * time.Second
-	case period < (1+5)*time.Minute:
-		// once every 30 seconds for next 5 minutes
-		delay = 30 * time.Second
-	case period < (1+5+10)*time.Minute:
-		// once every 1 minute for next 10 minutes
-		delay = time.Minute
-	default:
-		// once every 5 minutes therein
-		delay = 5 * time.Minute
+
+	if retry.firstRetry.IsZero() {
+		retry.reset()
+		delay = 0
+	} else if time.Now().Sub(retry.lastRetry) > time.Duration(20*time.Minute) {
+		// reset retry stats after 20 minutes of non-failures
+		retry.reset()
+		delay = 0
+	} else {
+		retryingDuration := time.Now().Sub(retry.firstRetry)
+		switch {
+		case retryingDuration < time.Minute:
+			// once every 5 seconds for 1 minute
+			delay = 5 * time.Second
+		case retryingDuration < (1+5)*time.Minute:
+			// once every 30 seconds for next 5 minutes
+			delay = 30 * time.Second
+		case retryingDuration < (1+5+10)*time.Minute:
+			// once every 1 minute for next 10 minutes
+			delay = time.Minute
+		default:
+			// once every 5 minutes therein
+			delay = 5 * time.Minute
+		}
 	}
+
 	if shouldWarn {
 		log.Warnf("%s; retrying after %v.", msg, delay)
 	} else {
 		log.Infof("%s; retrying after %v.", msg, delay)
 	}
 	time.Sleep(delay)
+	retry.lastRetry = time.Now()
 	return true
+}
+
+func (retry *InfiniteRetryer) reset() {
+	now := time.Now()
+	retry.firstRetry = now
+	retry.lastRetry = now
 }
