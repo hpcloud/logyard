ee669a60fef3b16cdc2ecffb85ef321dbc9c2917
Sridhar Ratnakumar <sridharr@activestate.com>
2013-04-05 12:54:45 -0700

    prove that hierarchical config works out of the box

--- util/confdis/confdis_test.go
+++ util/confdis/confdis_test.go
@@ -8,6 +8,10 @@ import (
 type SampleConfig struct {
 	Name  string   `json:"name"`
 	Users []string `json:"users"`
+	Meta  struct {
+		Researcher string `json:"researcher"`
+		Grant      int    `json:"grant"`
+	} `json:"meta"`
 }
 
 func TestSimple(t *testing.T) {
@@ -20,6 +24,8 @@ func TestSimple(t *testing.T) {
 	fmt.Println(config)
 	config.Name = "primates"
 	config.Users = []string{"chimp", "bonobo", "lemur"}
+	config.Meta.Researcher = "Jane Goodall"
+	config.Meta.Grant = 1200
 	c.Save()
 	c.reload()
 	fmt.Println(config)
