9903498cbfe17146e526caf192aeb3f6428d179a
Sridhar Ratnakumar <sridharr@activestate.com>
2013-10-25 18:01:08 -0700

    add syslog PRIORITY and TIMESTAMP fields to all log streams
    i.e., including systail, apptail and event.
    
    also add a named format "systail_syslog" with a fully constructed syslog format.

--- clients/messagecommon/messagecommon.go
+++ clients/messagecommon/messagecommon.go
@@ -7,17 +7,20 @@ import (
 // MessageCommon contains common fields required for representing any message stream
 type MessageCommon struct {
 	Text      string // Text content of the stream message
+	NodeID    string // IP address of the node from which this message originated
 	UnixTime  int64  // Unix timestamp
 	HumanTime string // Human readable time (unspecified format)
-	NodeID    string // IP address of the node from which this message originated
+	Syslog    syslogMessage
 }
 
 func New(text string, t time.Time, node string) MessageCommon {
 	return MessageCommon{
 		text,
+		node,
 		t.Unix(),
 		toHerokuTime(t),
-		node}
+		newSyslogMessage(DEFAULT_SYSLOG_PRIORITY, t),
+	}
 }
 
 // ToHerokuTime formats the given time using heroku's datetime format
---
+++ clients/messagecommon/syslogmessage.go
@@ -0,0 +1,17 @@
+package messagecommon
+
+import (
+	"time"
+)
+
+const DEFAULT_SYSLOG_PRIORITY = "13"
+
+// http://en.wikipedia.org/wiki/Syslog#Format_of_a_Syslog_Packet
+type syslogMessage struct {
+	Priority string
+	Time     string
+}
+
+func newSyslogMessage(pri string, t time.Time) syslogMessage {
+	return syslogMessage{pri, t.Format(time.Stamp)}
+}
