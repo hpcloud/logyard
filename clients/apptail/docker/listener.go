hpcloud/logyard

commit 5125f11d2a5ea41fdf02a1b335897ffa3cc5a6c4

account for full id used in docker 0.6.7 API
Bug #101814
(cherry picked from commit 5caa9786fa46cb41d282695c7421a7b22b65f961)

clients/apptail/docker/listener.go
@@ -48,8 +48,9 @@ func (l *dockerListener) BlockUntilContainerStops(id string) {
 
 func (l *dockerListener) Listen() {
 	for evt := range docker_events.Stream() {
-		if len(evt.Id) != ID_LENGTH {
-			common.Fatal("Invalid docker ID length: %v", len(evt.Id))
+		id := evt.Id[:ID_LENGTH]
+		if len(id) != ID_LENGTH {
+			common.Fatal("Invalid docker ID length: %v (orig: %v)", len(id), len(evt.Id))
 		}
 
 		// Notify container stop events by closing the appropriate ch.
@@ -57,7 +58,7 @@ func (l *dockerListener) Listen() {
 			continue
 		}
 		l.mux.Lock()
-		if ch, ok := l.waiters[evt.Id]; ok {
+		if ch, ok := l.waiters[id]; ok {
 			close(ch)
 			delete(l.waiters, evt.Id)
 		}

commit 3161686f9c55dd21013e0eca636c651797cc53e8

fix close-of-closed-ch panic introduced by 5125f11d

clients/apptail/docker/listener.go
@@ -60,7 +60,7 @@ func (l *dockerListener) Listen() {
 		l.mux.Lock()
 		if ch, ok := l.waiters[id]; ok {
 			close(ch)
-			delete(l.waiters, evt.Id)
+			delete(l.waiters, id)
 		}
 		l.mux.Unlock()
 	}