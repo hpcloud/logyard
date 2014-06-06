61db4d8cf4d72108c51f625611eb2d247241a1ab
Sridhar Ratnakumar <sridharr@activestate.com>
2014-06-05 19:39:40 -0700

    fail abruptly if /var/stackato/run doesn't exist
    https://bugs.activestate.com/show_bug.cgi?id=104006#c10

--- logyard.go
+++ logyard.go
@@ -3,6 +3,7 @@ package logyard
 import (
 	"github.com/ActiveState/log"
 	"github.com/ActiveState/zmqpubsub"
+	"os"
 )
 
 var Broker zmqpubsub.Broker
@@ -11,6 +12,11 @@ func init() {
 	Broker.PubAddr = "ipc:///var/stackato/run/logyardpub.sock"
 	Broker.SubAddr = "ipc:///var/stackato/run/logyardsub.sock"
 	Broker.BufferSize = 100
+
+	// ZeroMQ will silently fail if .sock files can't be created
+	if _, err := os.Stat("/var/stackato/run"); err != nil {
+		log.Fatalf("Cannot access /var/stackato/run: %v", err)
+	}
 
 	log.Infof("Loygard broker config: %+v\n", Broker)
 }
